8f860cdfd6c7319af12b1929a96265f6ffb4553c
Sam Kovacevic <samkovacevic05@gmail.com>
2023-08-09 11:51:48 +0200

    singleProducts

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,31 @@
 <?php
 declare(strict_types=1);
 
-echo 'It works !';
+
+//connexion a la DB
+
+try {
+    $pdo = new PDO(
+        'mysql:host=' . getenv('DB_HOST') . ';
+        dbname=' . getenv('DB_DATABASE'),
+        getenv('DB_USERNAME'),
+        getenv('DB_PASSWORD')
+    );
+
+    $sql = "SELECT * FROM products LIMIT 20";
+
+    $stmt = $pdo->query($sql);
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+} catch (Exception $e) {
+    print_r($e->getMessage());
+}
+
+//afficher les produits
+include 'public/view/layout/header.views.php';
+
+include 'public/view/layout/footer.views.php';
+
+include 'public/view/layout/index.views.php';
+
+include 'public/view/layout/singleProduct.views.php';
